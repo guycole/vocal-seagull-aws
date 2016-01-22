f5360682a39aab74aa33e371ab8d72cf6975971d
Guy Cole <guycole@gmail.com>
2016-01-22 23:48:25 +0000

    dump cleanup

--- vocal_seagull_py/wx_db_dump.py
+++ vocal_seagull_py/wx_db_dump.py
@@ -1,9 +1,8 @@
 #! /usr/bin/python
 #
 # Title:wx_db_dump.py
-# Description:dump mythic scorer and write to AWS S3
+# Description:dump vocal seagull and write to AWS S3
 # Development Environment:OS X 10.9.3/Python 2.7.7
-# Legalise:Copyright (C) 2014 Digital Burro, INC.
 # Author:G.S. Cole (guycole at gmail dot com)
 #
 import datetime
@@ -19,19 +18,6 @@ from boto.s3.key import Key
 
 
 class DumpDriver:
-
-    def writeToS3(self, outFileName, outFilePath):
-        bucketName = "mythic-scorer-%d-%2.2d" % (datetime.datetime.today().year, datetime.datetime.today().month)
-
-        bucketTest = s3connection.lookup(bucketName)
-        if bucketTest is None:
-            print "must create bucket:%s" % bucketName
-            s3connection.create_bucket(bucketName)
-
-        s3bucket = s3connection.get_bucket(bucketName)
-        s3key = Key(s3bucket)
-        s3key.key = outFileName
-        s3key.set_contents_from_filename(outFilePath)
 
     def execute(self, task_id):
         start_time = time.time()
